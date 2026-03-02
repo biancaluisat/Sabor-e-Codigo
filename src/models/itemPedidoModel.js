7820deec74ee811f874a6d9e713313a1e029b7f2
Murilow00 <milanmurilo7@gmail.com>
2026-03-02 09:57:34 -0300

    finaliza o create do ItemPedidoController

--- src/models/itemPedidoModel.js
+++ src/models/itemPedidoModel.js
@@ -1,29 +1,51 @@
 import prisma from '../utils/prismaClient.js';
 
-export default class itemPedidoModel {
-    constructor({id, pedidoId, pedido, produtoId, produto, quantidade, precoUnitario }){
-        this.id = id,
-        this.pedidoId= pedidoId,
-        this.pedido = pedido,
-        this.produtoId = produtoId,
-        this.produto = produto,
-        this.quantidade = quantidade,
-        this.precoUnitario = precoUnitario
+export default class ItemPedidoModel {
+    constructor({ id, pedidoId, produtoId, quantidade, precoUnitario }) {
+        this.id = id;
+        this.pedidoId = pedidoId;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
     }
+
     async criar() {
         return prisma.itemPedido.create({
-            data:{
-                pedido: this.pedido,
-                produto: this.produto,
+            data: {
+                pedido: { connect: { id: this.pedidoId } },
+                produto: { connect: { id: this.produtoId } },
                 quantidade: this.quantidade,
                 precoUnitario: this.precoUnitario
             }
         });
-    }
-    async atualizar () {
+    };
+
+    async atualizar() {
         return prisma.itemPedido.update({
-            where: {id: this.id},
-            data: {pedido: this.pedido, produto: this.produto, quantidade: this.quantidade, precoUnitario: this.precoUnitario}
+            where: { id: this.id },
+            data: {
+                pedido: { connect: { id: this.pedidoId } },
+                produto: { connect: { id: this.produtoId } },
+                quantidade: this.quantidade,
+                precoUnitario: this.precoUnitario
+            }
         });
+    };
+
+    async deletar() {
+        return prisma.itemPedido.delete({
+            where: { id: this.id }
+        });
+    };
+
+    static async buscarTodos() {
+        const data = await prisma.itemPedido.findMany();
+        return data.map(item => new ItemPedidoModel(item));
+    };
+
+    static async pegarPorId(id){
+        const data = await prisma.itemPedido.findUnique({ where: { id } });
+        if (!data) return null;
+        return new ItemPedidoModel(data);
     }
 }
--- src/models/pedidoModel.js
+++ src/models/pedidoModel.js
@@ -62,3 +62,4 @@ export default class PedidoModel {
 
         return itemCriado;
     }
+}
