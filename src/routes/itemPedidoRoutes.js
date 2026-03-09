4ae02bf697bd11edae00cab6e4ddb7aa598deaba
Murilow00 <milanmurilo7@gmail.com>
2026-03-09 10:50:34 -0300

    Conserta algumas mudanças no item pedido

--- src/routes/itemPedidoRoutes.js
+++ src/routes/itemPedidoRoutes.js
@@ -1,19 +1,15 @@
 import express from 'express';
 import * as controller from '../controllers/itemPedidoController.js';
-<<<<<<< yasmin
-import autenticarApiKey from "../utils/apiKey.js";
-=======
 import autenticarApiKey from '../utils/apiKey.js';
->>>>>>> main
+
 
 const router = express.Router();
 
 
 router.post('/itempedido', controller.criar);
-router.get('/itempedido', autenticarApiKey, controller.buscarTodos);
+router.get('/itempedido', controller.buscarTodos);
 router.get('/itempedido/:id', controller.buscarPorId);
 router.put('/itempedido/:id', controller.atualizar);
 router.delete('/itempedido/:id', controller.deletar);
 
 export default router;
-
